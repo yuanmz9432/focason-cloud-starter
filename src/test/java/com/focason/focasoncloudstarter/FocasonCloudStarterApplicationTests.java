7689294f1962231141cd202262c2f173fe627500
yuanmz9432 <yuanmz9432@gmail.com>
2025-12-11 23:44:25 +0900

    Initial commit

---
+++ src/test/java/com/focason/focasoncloudstarter/FocasonCloudStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focason.focasoncloudstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FocasonCloudStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
